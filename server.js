48f7ade5fa9b68036441e52f311952ef973a86fc
martin <martin@comets.kr>
2021-11-19 00:04:34 +0900

    feat: http server via core node packages

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("The server started");
+const moment = require("moment");
+console.log("The server started at ", moment().format("YYYY-MM-DD HH:mm"));
+const http = require("http");
+
+let soni = 0;
+
+let server = http.createServer((req, res) => {
+  soni++;
+  console.log("Kimdir kirdi bu yerga, serverda odam soni: ", soni);
+  res.end("Hello World");
+});
+
+server.listen(3003, () => {
+  console.log("server tayyor");
+});
